571183696831447ce719e4dfddbfb6f5f28cfb33
x18780682697 <x18780682697@gmail.com>
2022-08-10 09:08:56 +0800

    initial commit

---
+++ app/src/main/java/wen/example/constraintlayout/MainActivity.kt
@@ -0,0 +1,15 @@
+package wen.example.constraintlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.chain.TestChainActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, TestChainActivity::class.java))
+    }
+}
---
+++ app/src/main/java/wen/example/constraintlayout/chain/TestChainActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.constraintlayout.chain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestChainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_chain)
+    }
+}
