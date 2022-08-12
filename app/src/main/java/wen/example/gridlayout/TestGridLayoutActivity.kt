4f550701b8a7a7f2a662efcef12bc90911111545
alex <x18780682697@gmail.com>
2022-08-12 08:59:40 +0800

    GridLayout demo

---
+++ app/src/main/java/wen/example/gridlayout/TestGridLayoutActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.gridlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestGridLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_grid_layout)
+    }
+}
