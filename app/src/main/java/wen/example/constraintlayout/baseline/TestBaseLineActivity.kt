fe4cc43ae81424a5d5a0d1e8597b75d863a5cf8f
x18780682697 <x18780682697@gmail.com>
2022-08-13 22:00:42 +0800

    测试文本基线对齐特性

---
+++ app/src/main/java/wen/example/constraintlayout/baseline/TestBaseLineActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.constraintlayout.baseline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestBaseLineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_base_line)
+    }
+}
