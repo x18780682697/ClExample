1a5069ff2a8c909c71b4e4cdaece08e79bb45c4e
x18780682697 <x18780682697@gmail.com>
2022-08-13 22:24:09 +0800

    测试屏障约束特性

---
+++ app/src/main/java/wen/example/constraintlayout/barrier/TestBarrierActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.constraintlayout.barrier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestBarrierActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_barrier)
+    }
+}
