e77478734a975a8ad6eb1081d1d49c8aa748ec43
x18780682697 <x18780682697@gmail.com>
2022-08-14 17:50:24 +0800

    测试对齐统计试图特性

---
+++ app/src/main/java/wen/example/constraintlayout/align/TestAlignViewActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.constraintlayout.align
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestAlignViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_align_view)
+    }
+}
