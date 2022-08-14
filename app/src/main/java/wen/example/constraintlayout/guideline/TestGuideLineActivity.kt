b769ff2a39f778856370f3f28d453b55b9f29b80
x18780682697 <x18780682697@gmail.com>
2022-08-14 11:57:49 +0800

    测试引导线特性

---
+++ app/src/main/java/wen/example/constraintlayout/guideline/TestGuideLineActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.constraintlayout.guideline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestGuideLineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_guide_line)
+    }
+}
