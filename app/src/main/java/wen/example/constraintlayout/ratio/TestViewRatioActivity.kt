b2953494dcc0ec0cf37b1f54c8bf3689b05346d5
x18780682697 <x18780682697@gmail.com>
2022-08-14 12:25:58 +0800

    测试试图比例特性

---
+++ app/src/main/java/wen/example/constraintlayout/ratio/TestViewRatioActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.constraintlayout.ratio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestViewRatioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_view_ratio)
+    }
+}
