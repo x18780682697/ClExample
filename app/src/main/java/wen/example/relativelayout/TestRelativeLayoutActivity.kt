a85a309243d4c3a8dfd28576cc1534bfc10c706b
x18780682697 <x18780682697@gmail.com>
2022-08-16 10:19:25 +0800

    RelativeLayout常见用法示例

---
+++ app/src/main/java/wen/example/relativelayout/TestRelativeLayoutActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestRelativeLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_relative_layout)
+    }
+}
