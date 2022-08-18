92cdd52562c102befa84b88921d8793ba5088f44
alex <x18780682697@gmail.com>
2022-08-18 08:51:22 +0800

    LinearLayout基本功能示例

---
+++ app/src/main/java/wen/example/linearlayout/TestLinearLayoutActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestLinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_linear_layout)
+    }
+}
