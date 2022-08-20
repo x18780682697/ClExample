1944219794e6c1fa04db2f14fe8a64c1cd071870
x18780682697 <x18780682697@gmail.com>
2022-08-20 22:15:59 +0800

    ConstraintLayout实现负margin示例

---
+++ app/src/main/java/wen/example/constraintlayout/margin/TestNegativeMarginActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.constraintlayout.margin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestNegativeMarginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_negative_margin)
+    }
+}
