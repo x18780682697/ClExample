31120633043125a13c1d6c2e65e0a7d542a37a86
x18780682697 <x18780682697@gmail.com>
2022-08-20 21:03:02 +0800

    RelativeLayout实现负margin示例

---
+++ app/src/main/java/wen/example/relativelayout/TestRelativeLayoutNegativeMarginActivity.kt
@@ -0,0 +1,12 @@
+package wen.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wen.example.constraintlayout.R
+
+class TestRelativeLayoutNegativeMarginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_relative_layout_negative_margin)
+    }
+}
